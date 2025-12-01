a0b3d8885e2b6ae8e274f8e5257933a560590697
kodewala-academy <kodewala.academy@gmail.com>
2025-12-01 14:56:12 +0530

    Global Exception Handling

---
+++ RESTful-search-api/src/main/java/com/zepto/search/exception/ProductNotFoundExcpetion.java
@@ -0,0 +1,9 @@
+package com.zepto.search.exception;
+
+public class ProductNotFoundExcpetion extends RuntimeException {
+
+	public ProductNotFoundExcpetion(String _message) {
+		super(_message);
+	}
+
+}
---
+++ RESTful-search-api/src/main/java/com/zepto/search/serivce/ProductService.java
@@ -0,0 +1,21 @@
+package com.zepto.search.serivce;
+
+import org.springframework.stereotype.Service;
+
+import com.zepto.search.exception.ProductNotFoundExcpetion;
+
+@Service
+public class ProductService {
+
+	public void findProductBySearchString(String _searchString) {
+		boolean isProductFound = true;
+
+		if (isProductFound) {
+			// throw new ProductNotFoundExcpetion("Product not Found");
+			String s1 = null;
+			s1.length();
+		} else {
+			// send the product search results....
+		}
+	}
+}
