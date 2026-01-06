2bea1ce41c9aecb4d899f96110b9c184a645881c
kodewala-academy <kodewala.academy@gmail.com>
2026-01-06 16:28:37 +0530

    test

--- config-service/src/main/java/com/ecom/ConfigServiceApplication.java
+++ config-service/src/main/java/com/ecom/ConfigServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ConfigServiceApplication.main()");
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
